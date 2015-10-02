3b8a1b471ffe54141bc35a4bb88d1e64cdc77f7d
Nushrat Jahan Khan <njkhan2@illinois.edu>
2015-10-02 17:10:22 -0500

    ignore other status

--- lib/sead2/packager/extract_ore.rb
+++ lib/sead2/packager/extract_ore.rb
@@ -6,38 +6,74 @@ require 'rest-client'
 require 'cgi'
 require 'open-uri'
 
-researchobjects = RestClient.get 'https://sead-test.ncsa.illinois.edu/seadcp0.91/cp/repositories/Ideals/researchobjects'
+
+def login
+  host = 'http://localhost:8080'
+  user = 'njkhan505@gmail.com'
+  pwd = '123456'
+  begin
+    response = RestClient.post("#{host}/rest/login", {"email" => "#{user}", "password" => "#{pwd}"}.to_json,
+                               {:content_type => 'application/json',
+                                :accept => 'application/json'})
+    login_token = response.to_str
+    puts "Your login token is: #{login_token}"
+
+  rescue => e
+    puts "ERROR: #{e}"
+  end
+end
+
+login
+
+researchobjects = RestClient.get 'http://seadva-test.d2i.indiana.edu/sead-c3pr/api/repositories/ideals/researchobjects'
 researchobjects_parsed = JSON.parse(researchobjects)
-# puts researchobjects_parsed
+# p researchobjects_parsed
 
 researchobjects_parsed.each do |researchobject|
+  agg_id = "#{researchobject['Aggregation']['Identifier']}"
   stage = "#{researchobject['Status'][0]['stage']}"
   reporter = "#{researchobject['Status'][0]['reporter']}"
-  agg_id = "#{researchobject['Aggregation']['Identifier']}"
 
-  # p stage
 
-  if stage != "Receipt Ackowledged" || reporter != "SEAD-CP" then
+  old_item = false
+  researchobject['Status'].each do |status|
+    old_item = false unless status['stage'] == "Receipt Acknowledged" && status['reporter'] == "SEAD-CP"
+  end
+
+
+  if old_item then
     p "WARNING: Skipping #{agg_id}. Stage = #{stage}, Reporter = #{reporter}"
     next
   else
 
     agg_id_escaped = CGI.escape(agg_id)
     # p agg_id_escaped
-    ro_url = 'https://sead-test.ncsa.illinois.edu/seadcp0.91/cp/researchobjects/'+ agg_id_escaped
-    # p 'Retreive ORE from this link: '+ ro_url
+    ro_url = 'http://seadva-test.d2i.indiana.edu/sead-c3pr/api/researchobjects/'+ agg_id_escaped
+    # p 'Retreive Research Object from this link: '+ ro_url
+
+    updatestatus_url = "http://seadva-test.d2i.indiana.edu/sead-c3pr/api/researchobjects/#{agg_id_escaped}/status"
+
+    begin
+      # postStatus = RestClient.post("#{updatestatus_url}", {"reporter" => "Ideals", "stage" => "Pending"}.to_json, {:content_type => :json, :accept => :json})
+
+
+    rescue => e
+      p "ERROR: Cannot update status at #{updatestatus_url} (#{e})"
+      next
+    end
 
 
     ro_json = RestClient.get ro_url
+    # p ro_json
 
     if ro_json.code != 200
       p "ERROR: Invalid URL #{ro_url} -- #{ro_json.code}"
     end
 
     ro = JSON.parse(ro_json)
+    # p ro
     ore_url = ro["Aggregation"]["@id"]
-    # p id
-
+    p ore_url
 
 
     begin
@@ -55,16 +91,14 @@ researchobjects_parsed.each do |researchobject|
     ore = JSON.parse(ore_json)
     # p ore
 
-    # File.open("/Users/njkhan2/Desktop/test.jsonjd", "wb") do |saved_file|
-    #   open(ore, "rb") do |read_file|
-    #     saved_file.write(read_file.read)
-    #   end
-    # end
+    # ars = Array.new
+    # ars << ore
+    # p ars.count
 
+      File.open("/Users/njkhan2/Desktop/test.jsonjd", "wb") do |f|
+        f.write(ore.to_json)
+      end
 
-    File.open("/Users/njkhan2/Desktop/test.jsonjd", "wb") do |f|
-      f.write(ore.to_json)
-    end
 
     title = ore["describes"]["Title"]
     abstract = ore["describes"]["Abstract"]
@@ -72,23 +106,8 @@ researchobjects_parsed.each do |researchobject|
     creator = ore["describes"]["Creator"]
     date = ore["describes"]["Creation Date"]
     getFile = ore["describes"]["aggregates"][0]["similarTo"]
-    p "Abstract: " + abstract
-    p "Title: " + title
-    p "Creator: " + creator
-    p "Right: " + rights
-    p "date: " + date
-    p "URL for binary file: " + getFile
+    p "Abstract:  + #{abstract}\n Title: + #{title} \n Creator: + #{creator} \n Right: + #{rights} \n date: + #{date} \n URL for binary file: + #{getFile}"
 
-    updatestatus_url = "https://sead-test.ncsa.illinois.edu/seadcp0.91/cp/researchobjects/#{agg_id_escaped}/status"
-
-    begin
-      # postStatus = RestClient.post("#{updatestatus_url}", {"reporter" => "Ideals", "stage" => "Pending"}.to_json, {:content_type => :json, :accept => :json})
-
-
-    rescue => e
-      p "ERROR: Cannot update status at #{updatestatus_url} (#{e})"
-      next
-    end
 
 
     cookie_url = 'sead-test.ncsa.illinois.edu'
